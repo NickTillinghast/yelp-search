3dac0310bce9fac4c8ec2906ae1702d3f06e2771
Nick Tillinghast <nickt2@cox.net>
2020-06-14 22:55:00 -0700

    95

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,18 @@ import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
     return <View style={styles.backgroundStyle}>
         <Feather name="search" style={styles.iconStyle} />
-        <TextInput style={styles.inputStyle} placeholder="Search"></TextInput>
+        <TextInput
+            autoCapitalize="none"
+            autoCorrect={false}
+            style={styles.inputStyle}
+            placeholder="Search"
+            value={term}
+            onChangeText={onTermChange}
+            onEndEditing={onTermSubmit}
+        ></TextInput>
     </View>
 }
 
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from "../components/SearchBar";
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
+    const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+
+    const searchApi = async () => {
+        const response = await yelp.get('/search', {
+            params: {
+                limit: 50,
+                term,
+                location: 'phoenix'
+
+            }
+        });
+        setResults(response.data.businesses);
+    }
+
     return <View>
-        <SearchBar />
+        <SearchBar
+            term={term}
+            onTermChange={setTerm}
+            onTermSubmit={searchApi}
+        />
         <Text>Search Screen</Text>
+        <Text>We Have Found {results.length} results</Text>
+
     </View>
 }
 
