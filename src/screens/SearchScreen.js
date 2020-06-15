65fab1838127e8b210797dce935728b1b476ee1f
Nick Tillinghast <nickt2@cox.net>
2020-06-14 23:16:09 -0700

    98

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,26 +6,37 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('')
 
-    const searchApi = async () => {
-        const response = await yelp.get('/search', {
-            params: {
-                limit: 50,
-                term,
-                location: 'phoenix'
 
-            }
-        });
-        setResults(response.data.businesses);
+
+    const searchApi = async (searchTerm) => {
+        console.log('hit there')
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: searchTerm,
+                    location: 'phoenix'
+
+                }
+            });
+            setResults(response.data.businesses);
+        } catch (err) {
+            setErrorMessage('something went wrong')
+        }
     }
+    // call searchApi when component 
+    // is first rendered. BAD CODE
 
+    // searchApi('pasta')
     return <View>
         <SearchBar
             term={term}
             onTermChange={setTerm}
-            onTermSubmit={searchApi}
+            onTermSubmit={() => searchApi(term)}
         />
-        <Text>Search Screen</Text>
+        {errorMessage ? <Text>{errorMessage}</Text> : null}
         <Text>We Have Found {results.length} results</Text>
 
     </View>
